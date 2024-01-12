16a56a40baa93185c7167274bfe4ef883cdf846a
Stjefan <44638333+Stjefan@users.noreply.github.com>
2024-01-12 16:27:10 +0100

    changes

--- deployments.py
+++ deployments.py
@@ -1,30 +1,38 @@
 from prefect import flow, task, serve
 
 
+@flow(log_prints=True)
+def random_flow(repo_name: str = "PrefectHQ/prefect"):
+    print("Just a random_flow")
+    pass
+    
+
 
 if __name__ == "__main__":
-    flow.from_source(
+    random_flow.from_source(
         source="https://github.com/Stjefan/prefect-flows.git",
         entrypoint="flows.py:notify_people",
-    ).deploy(name="foo",
+    ).deploy(name="foo-azure",
         work_pool_name="my-managed-pool",
-        # cron="0 1 * * *"
+        cron="* * * * *"
         )
     
-    flow.from_source(
-        source="https://github.com/Stjefan/prefect-flows.git",
-        entrypoint="flows.py:send_a_mail",
-    ).deploy(name="bar",
-        work_pool_name="my-managed-pool",
-        )
+    # random_flow.from_source(
+    #     source="https://github.com/Stjefan/prefect-flows.git",
+    #     entrypoint="flows.py:send_a_mail",
+    # ).deploy(name="bar",
+    #     work_pool_name="my-managed-pool",
+
+        
+    #     )
     
-    flow.from_source(
-        source="https://github.com/discdiver/demos.git",
-        entrypoint="my_gh_workflow.py:repo_info",
-    ).deploy(
-        name="baz",
-        work_pool_name="my-managed-pool",
-    )
+    # random_flow.from_source(
+    #     source="https://github.com/discdiver/demos.git",
+    #     entrypoint="my_gh_workflow.py:repo_info",
+    # ).deploy(
+    #     name="baz",
+    #     work_pool_name="my-managed-pool",
+    # )
 
         
     
--- flows.py
+++ flows.py
@@ -1,5 +1,8 @@
 from prefect import flow, task, serve
-import os
+# import os
+import smtplib
+from email.message import EmailMessage
+
 
 
 @flow(log_prints=True)
@@ -9,15 +12,32 @@ def notify_people(people="Walter"):
     print("Goodbye")
 
 
+
 @flow(log_prints=True)
 def send_a_mail(people="Walter"):
     print("Hello from send_a_mail")
-    # Replace these with your details
+
+    receiver_email = "stefan.scheible@kurz-fischer.com"
+    port = 25
+    host = "syscp3.webhosting-franken.de"
+    server = smtplib.SMTP(host, port)
     sender_email = "alarm.svantek@kurz-fischer.de"
     sender_password = "kfnum@alarm7"
-    receiver_email = "stefan.scheible@kurz-fischer.com"
 
-    server = "syscp3.webhosting-franken.de"
+    msg = EmailMessage()
+    msg["Subject"] = "Hello from prefect"
+    msg.set_content("From a flow")
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    
+    
+    server.login(sender_email, sender_password)
+    server.send_message(msg)
+    server.quit()
+    # Replace these with your details
+    
+
+
     print("Goodbye")
 
     # Set up the MIME
