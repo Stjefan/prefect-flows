024f3715fcd2e7fbed7445942c308faf4d875168
Stjefan <44638333+Stjefan@users.noreply.github.com>
2024-01-12 16:59:30 +0100

    ...

--- deployments.py
+++ deployments.py
@@ -1,6 +1,8 @@
 from prefect import flow, task, serve
 
 
+### Somehow st.scheible@gmail.com is broken, use st.scheible+prefect@gmail.com
+
 @flow(log_prints=True)
 def random_flow(repo_name: str = "PrefectHQ/prefect"):
     print("Just a random_flow")
@@ -12,9 +14,9 @@ if __name__ == "__main__":
     random_flow.from_source(
         source="https://github.com/Stjefan/prefect-flows.git",
         entrypoint="flows.py:notify_people",
-    ).deploy(name="foo-azure",
+    ).deploy(name="deploy-again",
         work_pool_name="my-managed-pool",
-        cron="* * * * *"
+        cron="*/15 6-22 * * *"
         )
     
     # random_flow.from_source(
--- flows.py
+++ flows.py
@@ -16,30 +16,7 @@ def notify_people(people="Walter"):
 @flow(log_prints=True)
 def send_a_mail(people="Walter"):
     print("Hello from send_a_mail")
-
-    receiver_email = "stefan.scheible@kurz-fischer.com"
-    port = 25
-    host = "syscp3.webhosting-franken.de"
-    server = smtplib.SMTP(host, port)
-    sender_email = "alarm.svantek@kurz-fischer.de"
-    sender_password = "kfnum@alarm7"
-
-    msg = EmailMessage()
-    msg["Subject"] = "Hello from prefect"
-    msg.set_content("From a flow")
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
-    
-    
-    server.login(sender_email, sender_password)
-    server.send_message(msg)
-    server.quit()
-    # Replace these with your details
-    
-
-
     print("Goodbye")
-
     # Set up the MIME
     
 
